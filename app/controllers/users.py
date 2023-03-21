86eb97504f3a7cd28c0bfc773023a568626ee612
Ivan Cobb <ivanfcobb@gmail.com>
2023-03-20 21:08:53 -0400

    Added login and register pages worked on styling

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -7,22 +7,23 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 bcrypt = Bcrypt(app)
 
 
-@app.route('/', methods=['get'])
-def register_login():
+@app.route('/')
+def index():
     return render_template("index.html")
 
 
-@app.route('/dashboard', methods=['get'])
-def dashboard():
-    try:
-        user = User.get_by_id(session['user_id'])
-        return render_template("dashboard.html", user=user)
-    except KeyError:
-        return redirect('/')
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
+    return render_template("login.html")
+
+
+
+@app.route('/login_user', methods=['POST'])
+def login_user():
     data = {"email": request.form["email"]}
     user_in_db = User.get_by_email(data["email"])
     if not user_in_db:
@@ -32,47 +33,43 @@ def login():
         flash("Invalid Email/Password", "login")
         return redirect('/')
     session['user_id'] = user_in_db.id
-    # never render on a post!!!
-    return redirect("/dashboard")
+    return redirect("/paintings")
 
 
-@app.route('/logout', methods=['GET'])
+
+@app.route('/logout')
 def logout():
     session.clear()
     return redirect("/")
 
 
-@app.route('/create_user', methods=["POST"])
-def register():
-    try:
-        is_valid = True
-        if request.form['password'] != request.form['password_confirm']:
-            flash("Confrim Password Does Not Match", "register")
-            is_valid = False
-        if len(request.form['password']) < 4:
-            flash("Password Must Be More Than 5 Characters", "register")
-            is_valid = False
-        if len(request.form['fname']) < 2:
-            flash("First Name must be at least 2 characters.", "register")
-            is_valid = False
-        if len(request.form['lname']) < 2:
-            flash("Last Name must be at least 2 characters.", "register")
-            is_valid = False
-        if not EMAIL_REGEX.match(request.form['email']):
-            flash("Invalid email address!", "register")
-            is_valid = False
-        if is_valid == False:
-            return redirect('/')
+
+@app.route('/register_user', methods=["POST"])
+def register_user():       
+    if User.is_valid_user(request.form):
         pw_hash = bcrypt.generate_password_hash(request.form['password'])
-        print(pw_hash)
         data = {
-            "fname": request.form['fname'],
-            "lname": request.form['lname'],
-            "email": request.form['email'],
-            "password": pw_hash,
+        "fname": request.form['fname'],
+        "lname": request.form['lname'],
+        "email": request.form['email'],
+        "password": pw_hash,
         }
         user_id = User.save(data)
         session['user_id'] = user_id
-        return redirect("/dashboard")
-    except:
-        return redirect("/")
+        return redirect("/paintings")
+    else:
+        return redirect('/')
+    
+    
+    
+@app.route('/purchase', methods=["POST"])
+def purchase():
+    data = {
+        'user_id': session['user_id'],
+        'painting_id': request.form['painting_id']
+    }
+    User.add_purchase(data)
+    return redirect("/paintings")
+
+
+
