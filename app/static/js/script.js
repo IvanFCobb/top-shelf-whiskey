f140de1e7e667ddfb72a16df9bb8b614d3dd9eec
Ivan Cobb <ivanfcobb@gmail.com>
2023-03-22 21:38:28 -0400

    began transition from bootstrap to tailwind

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,13 +1,5 @@
-const navbarToggle = document.getElementById('navbar-toggle');
-const navbarMenu = document.getElementById('navbar-menu');
-const closeBtn = document.getElementById('close-btn');
-
-navbarToggle.addEventListener('click', () => {
-  navbarMenu.classList.toggle('active');
-  navbarToggle.classList.toggle('hide');
-});
-
-closeBtn.addEventListener('click', () => {
-  navbarMenu.classList.remove('active');
-  navbarToggle.classList.remove('hide');
+$(function () {
+  $(".toggle-menu").click(function () {
+    $(".navbar-menu").toggleClass("hidden");
+  });
 });
